4242475a565fcb35492ebc4b8c9353b0ee17a416
ThePythonGuy <45978337+ThePythonGuy3@users.noreply.github.com>
2021-07-19 02:12:51 +0200

    Update and rename Step1.sh to main.sh

--- main.sh
+++ main.sh
@@ -15,3 +15,17 @@ wget https://dl.google.com/android/repository/build-tools_r30.0.3-linux.zip
 mkdir build-tools
 
 unzip -j build-tools_r30.0.3-linux.zip -d build-tools
+
+cd bin
+
+./sdkmanager --sdk_root=~/android "platforms;android-30" "platform-tools"
+
+cd ~
+
+echo "export ANDROID_HOME=~/android" >> .bashrc
+
+echo "export PATH=$PATH:$ANDROID_HOME/build-tools" >> .bashrc
+
+export ANDROID_HOME=~/android
+
+export PATH=$PATH:$ANDROID_HOME/build-tools
